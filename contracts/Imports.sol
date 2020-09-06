544bd6d3d51a87309880cd6766529c5b6f62acab
Poli Paul <polipaul777@hotmail.com>
2020-09-06 19:49:13 +0200

    Fix linter warning

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.6.10;
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 // solhint-disable-next-line no-empty-blocks
-contract Imports {}
+contract Imports {
+
+}
