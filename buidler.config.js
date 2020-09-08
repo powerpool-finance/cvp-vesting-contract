5cff48b7c2df1e726aecac4d43edb1963bc77f1f
Poli Paul <polipaul777@hotmail.com>
2020-09-08 10:17:54 +0200

    Add contract size and gas used reporters

--- buidler.config.js
+++ buidler.config.js
@@ -2,19 +2,30 @@ const { usePlugin } = require('@nomiclabs/buidler/config');
 
 usePlugin('@nomiclabs/buidler-truffle5');
 usePlugin('solidity-coverage');
+usePlugin('buidler-contract-sizer');
+usePlugin('buidler-gas-reporter');
+
 
 const config = {
   analytics: {
     enabled: false,
   },
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: false,
+  },
   defaultNetwork: 'buidlerevm',
+  gasReporter: {
+    currency: 'USD',
+    enabled: !!(process.env.REPORT_GAS)
+  },
   mocha: {},
   networks: {
     buidlerevm: {
       chainId: 31337,
     },
     local: {
-      url: 'http://127.0.0.1:9545',
+      url: 'http://127.0.0.1:8545',
     },
     coverage: {
       url: 'http://127.0.0.1:8555',
