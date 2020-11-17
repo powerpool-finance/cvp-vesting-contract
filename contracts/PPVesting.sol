a2b3d2c1a1670888112a6bc4417e6ecb3ad09d07
Poli Paul <polipaul777@hotmail.com>
2020-11-17 16:32:26 +0200

    Move the end of vote accounting and the forced 0 balances from  endV to endT

--- contracts/PPVesting.sol
+++ contracts/PPVesting.sol
@@ -245,7 +245,7 @@ contract PPVesting is CvpInterface {
     }
 
     // (Vote vesting at this blockNumber has ended)
-    if (blockNumber > endV) {
+    if (blockNumber > endT) {
       return 0;
     }
 
@@ -342,7 +342,7 @@ contract PPVesting is CvpInterface {
    * @return The available amount for claim
    */
   function getAvailableVotes(uint256 _alreadyClaimed) public view returns (uint256) {
-    if (block.number > endV) {
+    if (block.number > endT) {
       return 0;
     }
     return getAvailable(block.number, startV, amountPerMember, durationV, _alreadyClaimed);
@@ -393,7 +393,7 @@ contract PPVesting is CvpInterface {
 
     uint256 votes = getAvailableVotes(member.alreadyClaimedVotes);
 
-    require(block.number <= endV, "PPVesting::claimVotes: Vote vesting has ended");
+    require(block.number <= endT, "PPVesting::claimVotes: Vote vesting has ended");
     require(votes > 0, "PPVesting::claimVotes: Nothing to claim");
 
     _claimVotes(_to, member, votes);
@@ -484,7 +484,7 @@ contract PPVesting is CvpInterface {
 
     uint256 votes = getAvailableVotes(member.alreadyClaimedVotes);
 
-    if (block.number <= endV) {
+    if (block.number <= endT) {
       _claimVotes(msg.sender, member, votes);
     }
 
