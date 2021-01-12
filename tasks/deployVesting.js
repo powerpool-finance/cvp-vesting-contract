515d70028c07497c45cb037292e830aacd9c928c
defi-dev <defi.dev@protonmail.com>
2021-01-12 20:45:16 +0200

    deploy new vesting version

--- tasks/deployVesting.js
+++ tasks/deployVesting.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 
 task('deploy-vesting', 'Deploy Vesting')
   .setAction(async () => {
-    const PPVesting = await artifacts.require('PPVesting');
+    const PPVesting = await artifacts.require('PPTimedVesting');
     const CVP = await artifacts.require('IERC20');
 
     const {web3} = PPVesting;
@@ -13,26 +13,18 @@ task('deploy-vesting', 'Deploy Vesting')
     const [deployer] = await web3.eth.getAccounts();
     const sendOptions = {from: deployer};
 
-    const members = getAddresses('testers-beta').concat(getAddresses('testers-gamma'));
-
-    const configByNetworkId = {
-      '1': {
-        cvpAddress: '0x38e4adb44ef08f22f5b5b76a8f0c2d0dcbe7dca1',
-        startV: 10966820,
-        durationV: 2372500,
-        startT: 12465437,
-        durationT: 3555500
-      },
-      '42': {
-        cvpAddress: '0x86D0FFCf65eE225217e0Fe85DDB2B79A8CE7eDE2',
-        startV: await getBlockNumber(300),
-        durationV: 5000,
-        startT: await getBlockNumber(600),
-        durationT: 5000
-      }
-    };
+    const members = getAddresses('testers-dump');
 
     const networkId = parseInt(await web3.eth.net.getId());
+
+    const startV = 1601509764;
+    const configByNetworkId = {
+      cvpAddress: networkId === 1 ? '0x38e4adb44ef08f22f5b5b76a8f0c2d0dcbe7dca1' : '0x86D0FFCf65eE225217e0Fe85DDB2B79A8CE7eDE2',
+      startV,
+      durationV: 31560000,
+      startT: startV + 15780000,
+      durationT: 47340000
+    };
     const config = configByNetworkId[networkId];
     const cvp = await CVP.at(config.cvpAddress);
 
@@ -67,19 +59,7 @@ task('deploy-vesting', 'Deploy Vesting')
       ])
     );
 
-    async function getBlockNumber(addBlocks) {
-      return (await web3.eth.getBlockNumber()) + addBlocks;
-    }
-
     function getAddresses(filename) {
-      const addresses = [];
-      const membersCsv = fs.readFileSync('data/' + filename + '.csv', {encoding: 'utf8'});
-      membersCsv.split(/\r\n|\r|\n/g).forEach((line) => {
-        const split = line.split(',');
-        if (web3.utils.isAddress(split[0])) {
-          addresses.push(split[0]);
-        }
-      });
-      return addresses;
+      return JSON.parse(fs.readFileSync('data/' + filename + '.json', {encoding: 'utf8'}));
     }
   });
